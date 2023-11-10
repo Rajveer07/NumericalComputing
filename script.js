6860089e0cf4ce75ae0a58aa3d547c63d65cb352
Rajveer07 <satyaveer72.anand@gmail.com>
2023-11-11 00:53:38 +0530

    added code js file to store algorithms

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 let canvas = document.getElementById("display");
 console.log(canvas)
 
+const ctx = canvas.getContext("2d");
 
+//adding console text
 
+function text(text,color,x,y,font="orbitron",fontSize = "10px"){
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.font = `${fontSize} ${font}`;
+    ctx.textAlign = 'center'
+    ctx.textBaseline = 'middle'
+    ctx.fillText(text , x, y);
+}
+
+function animate(){
+    ctx.clearRect(0,0,canvas.clientWidth,canvas.height)
+    text("Display","green",16,10);
+    requestAnimationFrame(animate);
+}
+animate();
 
 
 
